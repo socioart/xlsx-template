9fb45adb6ce202a4f060611efafbdaeb813bc7f2
labocho <labocho@penguinlab.jp>
2021-02-18 14:04:23 +0900

    rename App to XlsxTemplate, make jar executable

---
+++ README.md
@@ -0,0 +1,4 @@
+# XlsxTemplate
+
+    mvn package
+    java -jar target/xlsx-template-1.0-SNAPSHOT.jar
--- src/main/java/com/socioart/XlsxTemplate.java
+++ src/main/java/com/socioart/XlsxTemplate.java
@@ -4,7 +4,7 @@ package com.socioart;
  * Hello world!
  *
  */
-public class App 
+public class XlsxTemplate
 {
     public static void main( String[] args )
     {
