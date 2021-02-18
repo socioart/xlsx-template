833f7aa3a256707b93d4459e65d0dde08312c3d2
labocho <labocho@penguinlab.jp>
2021-02-18 17:01:01 +0900

    replace variable with json

--- src/main/java/com/socioart/XlsxTemplate.java
+++ src/main/java/com/socioart/XlsxTemplate.java
@@ -5,6 +5,7 @@ import java.io.*;
 import java.util.Iterator;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import org.json.*;
 
 /**
  * Hello world!
@@ -14,21 +15,35 @@ public class XlsxTemplate
 {
     public static void main( String[] args ) throws IOException
     {
-        FileInputStream fis;
+        FileInputStream input_fis;
+        FileInputStream json_fis;
+
         String input = args[0];
         String output = args[1];
-        // String json = args[2];
+        String json_file = args[2];
         Pattern pattern = Pattern.compile("\\{\\{(.*)\\}\\}");
 
         try {
-            fis = new FileInputStream(input);
+            input_fis = new FileInputStream(input);
         } catch(FileNotFoundException e) {
-            System.err.println( "File not found" );
+            System.err.println( "Templatel file not found" );
             System.exit(1);
             return;
         }
 
-        XSSFWorkbook workbook = new XSSFWorkbookFactory().create(fis);
+        try {
+            json_fis = new FileInputStream(json_file);
+        } catch(FileNotFoundException e) {
+            System.err.println( "Data file not found" );
+            System.exit(1);
+            input_fis.close();
+            return;
+        }
+
+        JSONObject variables = new JSONObject(new String(json_fis.readAllBytes()));
+        json_fis.close();
+
+        XSSFWorkbook workbook = new XSSFWorkbookFactory().create(input_fis);
         Iterator<Sheet> sheets = workbook.sheetIterator();
 
         while(sheets.hasNext()) {
@@ -51,8 +66,8 @@ public class XlsxTemplate
                             Matcher m = pattern.matcher(value);
                             if (m.find()) {
                                 String variable = m.group(1);
-                                String[] parts = variable.split("\\.");
-                                cell.setCellValue(String.join("->", parts));
+                                String query = "/" + variable.replaceAll("\\.", "/");
+                                cell.setCellValue(dig(variables, query));
                             }
                             break;
                         default:
@@ -65,5 +80,9 @@ public class XlsxTemplate
 
         FileOutputStream fos = new FileOutputStream(output);
         workbook.write(fos);
+    }
+
+    public static String dig(JSONObject variables, String query) {
+        return variables.query(query).toString();
     }
 }
