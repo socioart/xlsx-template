fc582252430fe5802a0fe4e711075690367ba34a
labocho <labocho@penguinlab.jp>
2021-02-18 17:12:59 +0900

    refactoring

--- src/main/java/com/socioart/XlsxTemplate.java
+++ src/main/java/com/socioart/XlsxTemplate.java
@@ -13,6 +13,62 @@ import org.json.*;
  */
 public class XlsxTemplate
 {
+    static class Template {
+        private Pattern _pattern;
+        private XSSFWorkbook _template;
+        private JSONObject _variables;
+
+        public Template() {
+            _pattern = Pattern.compile("\\{\\{(.*)\\}\\}");
+        }
+
+        public void render(XSSFWorkbook template, JSONObject variables, String output) throws IOException {
+            _template = template;
+            _variables = variables;
+
+            Iterator<Sheet> sheets = _template.sheetIterator();
+
+            while(sheets.hasNext()) {
+                Sheet sheet = sheets.next();
+                System.out.println(sheet.getSheetName());
+                int row_index = -1;
+                while(true) {
+                    row_index += 1;
+
+                    Row row = sheet.getRow(row_index);
+                    if (row == null) break;
+
+                    Iterator<Cell> cells = row.cellIterator();
+                    while(cells.hasNext()) {
+                        Cell cell = cells.next();
+
+                        switch(cell.getCellType()) {
+                            case STRING:
+                                String value = cell.getStringCellValue();
+                                Matcher m = _pattern.matcher(value);
+                                if (m.find()) {
+                                    String variable = m.group(1);
+                                    String query = "/" + variable.replaceAll("\\.", "/");
+                                    cell.setCellValue(dig(query));
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+
+                }
+            }
+
+            FileOutputStream fos = new FileOutputStream(output);
+            _template.write(fos);
+        }
+
+        private String dig(String query) {
+            return _variables.query(query).toString();
+        }
+    }
+
     public static void main( String[] args ) throws IOException
     {
         FileInputStream input_fis;
@@ -21,7 +77,6 @@ public class XlsxTemplate
         String input = args[0];
         String output = args[1];
         String json_file = args[2];
-        Pattern pattern = Pattern.compile("\\{\\{(.*)\\}\\}");
 
         try {
             input_fis = new FileInputStream(input);
@@ -44,45 +99,9 @@ public class XlsxTemplate
         json_fis.close();
 
         XSSFWorkbook workbook = new XSSFWorkbookFactory().create(input_fis);
-        Iterator<Sheet> sheets = workbook.sheetIterator();
 
-        while(sheets.hasNext()) {
-            Sheet sheet = sheets.next();
-            System.out.println(sheet.getSheetName());
-            int row_index = -1;
-            while(true) {
-                row_index += 1;
-
-                Row row = sheet.getRow(row_index);
-                if (row == null) break;
-
-                Iterator<Cell> cells = row.cellIterator();
-                while(cells.hasNext()) {
-                    Cell cell = cells.next();
-
-                    switch(cell.getCellType()) {
-                        case STRING:
-                            String value = cell.getStringCellValue();
-                            Matcher m = pattern.matcher(value);
-                            if (m.find()) {
-                                String variable = m.group(1);
-                                String query = "/" + variable.replaceAll("\\.", "/");
-                                cell.setCellValue(dig(variables, query));
-                            }
-                            break;
-                        default:
-                            break;
-                    }
-                }
-
-            }
-        }
-
-        FileOutputStream fos = new FileOutputStream(output);
-        workbook.write(fos);
+        Template template = new Template();
+        template.render(workbook, variables, output);
     }
 
-    public static String dig(JSONObject variables, String query) {
-        return variables.query(query).toString();
-    }
 }
